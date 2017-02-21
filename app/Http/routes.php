2b96a3abac29ac319d90beaad2cf163a666f3d73
quang duy <quangduyx188@gmail.com>
2017-02-21 20:26:54 +0700

    google-drive-api

---
+++ app/Http/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+// This is where the user can see a login button for logging into Google
+Route::get('/', 'HomeController@index');
+
+// This is where the user gets redirected upon clicking the login button on the home page
+Route::get('/login', 'HomeController@login');
+
+// Shows a list of things that the user can do in the app
+Route::get('/dashboard', 'AdminController@index');
+
+// Shows a list of files in the users' Google drive
+Route::get('/files', 'AdminController@files');
+
+// Allows the user to search for a file in the Google drive
+Route::get('/search', 'AdminController@search');
+
+// Allows the user to upload new files
+Route::get('/upload', 'AdminController@upload');
+Route::post('/upload', 'AdminController@doUpload');
+
+// Allows the user to delete a file
+Route::get('/delete/{id}', 'AdminController@delete');
+
+Route::get('/logout', 'AdminController@logout');
