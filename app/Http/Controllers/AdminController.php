4e91697a763549722af119f67ab5c17da4aaabf7
hiennv94 <hiennv.th@gmail.com>
2017-03-02 19:14:56 +0700

    Improve code

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -38,7 +38,7 @@ class AdminController extends Controller
 
     public function crawlerImages()
     {
-        $this->crawlerFromWebsiteService->crapTest();
+        $this->crawlerFromWebsiteService->getList();
         return view('admin.dashboard');
     }
 
--- app/Services/CrawlerFromWebsiteService.php
+++ app/Services/CrawlerFromWebsiteService.php
@@ -26,22 +26,33 @@ class CrawlerFromWebsiteService
     public function getList()
     {
         $count = 0;
+        $listLink = config('image.list_link');
+        foreach ($listLink as $key => $value) {
+            if(!is_null($value))
+            {
+                $domConvert = $this->getDom($value);
+                $domPagination= $domConvert->find('//*[@id="pagination"]');
+                dd($domPagination);
+            }
+        }
+        // $domConvert = $this->getDom()
         for($i = 8; $i > 0; $i--) {
             $dom = $this->getDom("http://iphonewalls.net/celebrities-girls/page/".strval($i)."/");
+            dd($dom);
             foreach ($dom->find('//*[@id="wallpapers"]/div/article[*]/figure/a/img') as $link) {
                 $tmp = explode('.', $link->src);
                 $format = end($tmp);
                 $userImage = 'girls_'. $count .'.' .$format;
-                $path = '/home/likewise-open/FRAMGIA/nguyen.quang.duy/Google_driver/Women/';
-                $link_image = str_replace(" ","%20", preg_replace( "/-\d+x\d+/", "", $link->src));
-                $ch = curl_init($link_image);
-                $fp = fopen($path . $userImage, 'wb');
-                curl_setopt($ch, CURLOPT_FILE, $fp);
-                curl_setopt($ch, CURLOPT_HEADER, 0);
-                $result = curl_exec($ch);
-                curl_close($ch);
-                fclose($fp);
-                $count++;
+                // $path = '/home/likewise-open/FRAMGIA/'.env('LOCAL_COMPUTER').'/Google_driver/Women/';
+                // $link_image = str_replace(" ","%20", preg_replace( "/-\d+x\d+/", "", $link->src));
+                // $ch = curl_init($link_image);
+                // $fp = fopen($path . $userImage, 'wb');
+                // curl_setopt($ch, CURLOPT_FILE, $fp);
+                // curl_setopt($ch, CURLOPT_HEADER, 0);
+                // $result = curl_exec($ch);
+                // curl_close($ch);
+                // fclose($fp);
+                // $count++;
             }
         }
     }
